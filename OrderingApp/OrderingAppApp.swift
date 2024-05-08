3f5516ce79833f560316112a8a6477f61325241e
Ashes-1 <148441139+Ashes-1@users.noreply.github.com>
2024-05-08 17:14:24 +1000

    Initial Commit

---
+++ OrderingApp/OrderingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderingAppApp.swift
+//  OrderingApp
+//
+//  Created by Rosa Long on 8/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct OrderingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
